7e69787f9413214d222c5bd7dba1004faf1441bf
2211005 <144830831+2211005@users.noreply.github.com>
2023-11-28 08:19:12 -0500

    Update validar.php

--- GambitGallery/GambitGallery - copia/validar.php
+++ GambitGallery/GambitGallery - copia/validar.php
@@ -16,11 +16,11 @@ if ($conn->connect_error) {
 
 // Consultar la base de datos para el usuario
 $usuario = $_POST['username'];
-$clave_acceso = $_POST['password'];
+$password = $_POST['password'];
 
 // Consultar la base de datos para el usuario
-$sql = "SELECT id_usuario, usuario, clave_acceso FROM usuarios WHERE usuario = '$usuario'";
-$sql2 = "SELECT id_usuario, usuario, clave_acceso FROM usuarios WHERE clave_acceso = '$clave_acceso'";
+$sql = "SELECT id, username, email, password FROM usuarios WHERE usuario = '$usuario'";
+$sql2 = "SELECT id, username, email, password FROM usuarios WHERE clave_acceso = '$password'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // Usuario encontrado, verificar la contraseña
