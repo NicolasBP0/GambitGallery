04c7317407fe263dd09c0ecdb79a475ca2d5ed26
2211005 <144830831+2211005@users.noreply.github.com>
2023-12-04 17:31:16 -0500

    update

--- GambitGallery/GambitGallery - copia/validar.php
+++ GambitGallery/GambitGallery - copia/validar.php
@@ -17,15 +17,18 @@ if ($conn->connect_error) {
 // Consultar la base de datos para el usuario
 $usuario = $_POST['username'];
 $password = $_POST['password'];
+$passwordN = md5($password);
 
 // Consultar la base de datos para el usuario
 $sql = "SELECT id, username, email, password FROM users WHERE usuario = '$usuario'";
-$sql2 = "SELECT id, username, email, password FROM users WHERE clave_acceso = '$password'";
+$sql = mysqli_query($conn,"SELECT password FROM users WHERE nickname = '$usuario'");
+$mostrar = mysqli_fetch_array($sql);
 $result = $conn->query($sql);
+
 if ($result->num_rows > 0) {
     // Usuario encontrado, verificar la contraseña
     $row = $result->fetch_assoc();
-    if ($sql == $sql2) {
+    if ($passwordN == $mostrar['password']) {
         // Contraseña válida, iniciar sesión
         header("Location: index.html"); // Redirige al dashboard o a la página principal
         exit();
